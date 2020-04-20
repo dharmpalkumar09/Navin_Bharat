13b23881ab392a2ce4836a7d7f75758093ad96fd
Dharmpal kumar <dharmpal.kumar@aujas.com>
2020-04-20 10:55:13 +0530

    Project Initilised

---
+++ src/main/java/com/navinbharat/mediator/NavinbharatApplication.java
@@ -0,0 +1,13 @@
+package com.navinbharat.mediator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavinbharatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NavinbharatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/navinbharat/mediator/NavinbharatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navinbharat.mediator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NavinbharatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
